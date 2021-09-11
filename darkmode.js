57c31de5e10239caf8d988bc0d6231cda3aa2796
BertaHdr <gui.m.berta16@gmail.com>
2021-09-11 20:35:23 -0300

    direcinando varcss para js

--- darkmode.js
+++ darkmode.js
@@ -1,2 +1,9 @@
 const html = document.querySelector("html");
 const checkbox = document.querySelector("input[name=tema]");
+
+const coresIniciais = {
+    background: window.getComputedStyle(html).getPropertyValue("--background"),
+    corTitulo: window.getComputedStyle(html).getPropertyValue("--cortitulo"),
+    corTexto: window.getComputedStyle(html).getPropertyValue("--cortexto"),
+    header: window.getComputedStyle(html).getPropertyValue("--header"),
+}
