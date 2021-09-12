923e079442a0475d6b4224867f1adb6464e7a502
Gabriel Schirmer <gabriel.schirmer@sou.unijui.edu.br>
2021-09-12 03:54:20 -0300

    added çight and dark mode on about page, also minimized the hovering effects on navbar

---
+++ gabriel/public/js/about.js
@@ -0,0 +1,65 @@
+var body = $('body');
+var box = $('#box');
+var btn = $('#btn');
+// console.log(bodyClass);
+var changeButton = $("#mode-button");
+var buttonValue = $("#modeCheck").text();
+var modeCookie = $.cookie("mode");
+
+$(function(){
+    if (modeCookie == undefined) {
+        $.cookie("mode" , "dark");
+        $("#modeCheck").text("dark");
+
+    } else if ($.cookie("mode") == "dark") {
+        console.log("site abert: cookie" + $.cookie("mode"));
+        changeMode($.cookie("mode"));
+        $("#modeCheck").text("dark");
+
+    } else if ($.cookie("mode") == "light") {
+        console.log("site abert: cookie" + $.cookie("mode"));
+        changeMode($.cookie("mode"));
+        $("#modeCheck").text("light");
+    }
+    buttonValue = $("#modeCheck").text();
+});
+
+changeButton.on("click" , function(){
+
+    if (buttonValue == "dark") {
+        $("#modeCheck").text("light");
+        buttonValue = $("#modeCheck").text();
+        console.log("agora é:" + buttonValue);
+
+    } else {
+        $("#modeCheck").text("dark");
+        buttonValue = $("#modeCheck").text();
+        console.log("agora é:" + buttonValue);
+    }
+    changeMode(buttonValue);
+});
+
+function changeMode(currentMode) {
+    if (currentMode == "dark") {
+        body.removeClass("light-pages");
+        box.removeClass("bckg-box-light");
+        btn.removeClass("page-buttons-light");
+
+        body.toggleClass("dark-pages");
+        box.toggleClass("bckg-box-dark");
+        btn.toggleClass("page-buttons-dark");
+
+        $.cookie("mode" , "dark");
+    } 
+    if(currentMode == "light") {
+        body.removeClass("dark-pages");
+        box.removeClass("bckg-box-dark");
+        btn.removeClass("page-buttons-dark");
+
+        body.toggleClass("light-pages");
+        box.toggleClass("bckg-box-light");
+        btn.toggleClass("page-buttons-light");
+
+        $.cookie("mode" , "light");
+    }
+}
