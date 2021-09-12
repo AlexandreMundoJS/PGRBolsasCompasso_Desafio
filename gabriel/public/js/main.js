b55a885e9bfb91a85c683eea787fd397b4ff7f60
Gabriel Schirmer <gabriel.schirmer@sou.unijui.edu.br>
2021-09-12 01:55:37 -0300

    working on light and dark mode

--- gabriel/public/js/main.js
+++ gabriel/public/js/main.js
@@ -1,18 +1,36 @@
-var body = $("body");
-var bodyClass = $("body").attr('class');
-console.log(bodyClass);
-var modeCookie = $.cookie("mode");
-console.log("cookie:" + modeCookie);
-
+var body = $('body');
+// console.log(bodyClass);
 var changeButton = $("#mode-button");
+var buttonValue = $("#modeCheck").text();
 
 $(function(){
+    if (modeCookie == undefined) {
+        $.cookie("mode" , "dark");
+        console.log("adasdasdad");
+    }
+    changeMode(buttonValue);
 
 });
+var modeCookie = $.cookie("mode");
 
-function changeMode(){
-    changeButton.on("click" , function(){
-        body.removeClass("landing-dark-mode");
-        body.addClass("landing-dark-mode");
-    });
+changeButton.on("click" , function(){
+
+    if (buttonValue == "dark") {
+        $("#modeCheck").text("light");
+        buttonValue = $("#modeCheck").text();
+        console.log("agora é:" + buttonValue);
+    } else {
+        $("#modeCheck").text("dark");
+        buttonValue = $("#modeCheck").text();
+        console.log("agora é:" + buttonValue);
+    }
+    changeMode(buttonValue);
+});
+
+function changeMode(currentMode) {
+    if (currentMode = "dark") {
+        body.toggleClass("landing-dark-mode");
+    } else if (currentMode = "light") {
+        body.toggleClass("landing-light-mode");
+    }
 }
