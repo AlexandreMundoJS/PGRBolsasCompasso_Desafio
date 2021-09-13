db36c10b2f7183b2da4e79a9ebda7d6cb0521a4a
Anderson Severo <severowz@gmail.com>
2021-09-13 20:15:38 -0300

    Change button that hides form and some responsive properties

--- desafio-2/anderson/js/form.js
+++ desafio-2/anderson/js/form.js
@@ -1,32 +1,35 @@
 const nameVal = document.getElementById('name');
 const emailVal = document.getElementById('email');
+const subjectVal = document.getElementById('subject');
 const contentVal = document.getElementById('content');
 const formVal = document.getElementById('form');
 const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (submitForm) => {
-    let messages = []
+
+    let messages = [];
 
     if (nameVal.value === '' || nameVal.value == null) {
-      messages.push('O preenchimento do nome é obrigatório')
+      messages.push('O preenchimento do nome é obrigatório');
     }
 
     if (emailVal.value === '' || nameVal.value == null) {
-        messages.push('O preenchimento do campo e-mail é obrigatório')
+        messages.push('é obrigatório preencher o email');
+      }
+
+    if (subjectVal.value === '' || subjectVal.value == null) {
+        messages.push('é obrigatório preencher o assunto');
       }
 
     if (contentVal.value === '' || contentVal.value == null) {
-        messages.push('O preenchimento do campo assunto é obrigatório')
+        messages.push('é obrigatório enviar uma mensagem');
       }
 
-    if (contentVal.value === '' || contentVal.value == null) {
-        messages.push('O preenchimento do campo de mensagem é obrigatório')
-      }
-  
-
-   
     if(messages.length > 0) {
         submitForm.preventDefault();
         errorElement.innerText = messages.join(', ')
     }
+   
 })
+
+
--- desafio-2/anderson/js/script.js
+++ desafio-2/anderson/js/script.js
@@ -2,16 +2,26 @@ $(function(){
     $('.tooltip').tooltipster();
 });
 
-$(document).ready(function(){
-
-    $('.slide').click(function(){
-        $("#contato-id").slideToggle();
-        $("#error").slideToggle();
-
+ $(document).ready(function(){
+    $('#contact-button'). click(function(){
+        $(".form").toggleClass('form-opacity');
+        $("#error").toggleClass('form-opacity');
     });
-});
+    
+}); 
 
 
+// Testes
+
+/*  $('.contact-button').click(function(){
+        $(".form").toggleClass('form-opacity');
+        $("#error").toggleClass('form-opacity');
+    }); */
+
+
+    /*  $(".form").slideToggle();
+        $(".form").toggleClass('form-opacity'); 
+        */
 
 
 
